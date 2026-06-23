bab04eec2d5c29c017242757d31ef4f02bdb59b3
hussain <hussbelf@gmail.com>
2026-06-23 09:51:54 -0700

    push dist

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 import AutoImport from "unplugin-auto-import/vite";
+// import { readdyJsxRuntimeProxyPlugin } from "./vite.jsx-runtime-proxy";
 
 const base = process.env.BASE_PATH || "/";
 const isPreview = process.env.IS_PREVIEW ? true : false;
-
+//const proxyPlugins = isPreview ? [readdyJsxRuntimeProxyPlugin()] : [];
 // https://vite.dev/config/
 export default defineConfig({
   define: {
@@ -16,6 +17,7 @@ export default defineConfig({
     __READDY_AI_DOMAIN__: JSON.stringify(process.env.READDY_AI_DOMAIN || ""),
   },
   plugins: [
+    // ...proxyPlugins,
     react(),
     AutoImport({
       imports: [
@@ -70,18 +72,7 @@ export default defineConfig({
   base,
   build: {
     sourcemap: true,
-    outDir: 'out',
-    // التعديل الجديد هنا لتفتيت الملف الكبير لحزم أصغر
-    rolldownOptions: {
-      output: {
-        manualChunks(id) {
-          // فصل كل مكتبات الـ node_modules في ملف منفصل يسمى vendor
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
+    outDir: 'dist',
   },
   resolve: {
     alias: {
